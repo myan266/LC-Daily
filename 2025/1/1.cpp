e110046e86580eca440250cc8fb5ebb641ed1065
myan266 <myan266@cs.washington.edu>
2024-12-31 16:17:22 -0800

    Update 1.cpp
    one-pass solution

--- 2025/1/1.cpp
+++ 2025/1/1.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ocnt = count(s.begin(), s.end(), '1'), zcnt = 0;
+        int ocnt = 0, zcnt = 0;
 
-        int maxScore = 0;
+        int maxScore = -1000;
         for (int i = 0; i < s.size()-1; i++) {
-            int curScore = (zcnt += (s[i] == '0')) + (ocnt -= (s[i] == '1'));
+            int curScore = (zcnt += (s[i] == '0')) - (ocnt += (s[i] == '1'));
             maxScore = max(maxScore, curScore);
         }
 
-        return maxScore;
+        return maxScore + ocnt + (s[s.size() - 1] == '1');
     }
 };
