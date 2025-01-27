c115a145d18644ee497d95c7af02803688e15240
myan266 <myan266@cs.washington.edu>
2025-01-27 11:09:33 -0800

    Update 27.cpp

--- 2025/1/27.cpp
+++ 2025/1/27.cpp
@@ -1,3 +1,4 @@
+// Modified Floyd-Warshall
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prereq, vector<vector<int>>& queries) {
@@ -27,5 +28,43 @@ public:
 
         return ans;
 
+    }
+};
+
+// Topological sort solution with bitmask
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prereq, vector<vector<int>>& queries) {
+        // A is a prereq of B <-> there exists a path in the DAG from A to B
+        vector<bitset<100>> preq(n);
+        vector<vector<int>> graph(n);
+        vector<int> inDeg(n);
+
+        for (vector<int> &pre : prereq) {
+            preq[pre[1]].set(pre[0], 1);
+            graph[pre[0]].push_back(pre[1]);
+            inDeg[pre[1]]++;
+        }
+
+        queue<int> topo;
+
+        for (int i = 0; i < n; i++) {
+            if (inDeg[i] == 0) topo.push(i);
+        }
+        
+        while (!topo.empty()) {
+            int cur = topo.front(); topo.pop();
+            for (int v : graph[cur]) {
+                preq[v] |= preq[cur];
+                inDeg[v]--; 
+                if (inDeg[v] == 0) topo.push(v);
+            }
+        }
+
+        int m = queries.size();
+        vector<bool> ans(m);
+        for (int i = 0; i < m; i++) ans[i] = preq[queries[i][1]][queries[i][0]];
+
+        return ans;
     }
 };
