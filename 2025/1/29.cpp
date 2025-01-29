21b27ef3e086e883044447e9e8a9070ab3463ca8
myan266 <myan266@cs.washington.edu>
2025-01-28 17:07:38 -0800

    Update 29.cpp

--- 2025/1/29.cpp
+++ 2025/1/29.cpp
@@ -1,3 +1,4 @@
+// classic DSU
 class Solution {
 private:
     const static int maxn = 1002;
@@ -32,5 +33,42 @@ public:
             if (!join(e[0], e[1])) ans = e;
         }
         return ans;
+    }
+};
+
+// DFS Cycle Detection
+class Solution {
+private:
+    const static int maxn = 1002;
+    vector<int> graph[maxn];
+    bool isInCycle[maxn];
+    int vis[maxn];
+
+    void dfs(int cur, int par) {
+        if (vis[cur] != 0) { // cycle detected
+            int st = cur;
+            vis[st] = par;
+            do {
+                isInCycle[cur] = 1;
+                cur = vis[cur];
+            } while (cur != st);
+            return;
+        }
+        vis[cur] = par;
+        for (int v : graph[cur]) {
+            if (v != par) dfs(v, cur);
+        }
+    }
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        for (vector<int> &e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        dfs(1, -1);
+        for (int i = edges.size() - 1; i >= 0; i--) {
+            if (isInCycle[edges[i][0]] && isInCycle[edges[i][1]]) return edges[i];
+        }
+        return {};
     }
 };
